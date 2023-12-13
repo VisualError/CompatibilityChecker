6107afd09e18f3da9e9b3fea2c8df7c227161166
Vis <adominiqjakob1225@gmail.com>
2023-12-13 15:02:59 +0800

    Initial commit

---
+++ Netcode/PlayerJoinNetcode.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Steamworks;
+using Steamworks.Data;
+using Unity.Netcode;
+using UnityEngine;
+using System.Linq;
+
+namespace CompatibilityChecker.Netcode
+{
+    [HarmonyPatch]
+    class PlayerJoinNetcode
+    {
+        static string[] serverModList = null;
+        [HarmonyPatch(typeof(GameNetworkManager), "SteamMatchmaking_OnLobbyCreated")]
+        [HarmonyPrefix]
+        public static bool OnLobbyCreated(Result result, ref Lobby lobby)
+        {
+            if(result == Result.OK)
+            {
+                lobby.SetData("mods", ModNotifyBase.ModListString);
+                ModNotifyBase.logger.LogInfo($"Set lobby mods to: {ModNotifyBase.ModListString}");
+            }
+            return true;
+        }
+        [HarmonyPatch(typeof(GameNetworkManager), "LobbyDataIsJoinable")]
+        [HarmonyPrefix]
+        public static bool IsJoinable(ref Lobby lobby)
+        {
+            string mods = lobby.GetData("mods");
+            if (!mods.IsNullOrWhiteSpace())
+            {
+                ModNotifyBase.logger.LogInfo("Lobby is modded. Getting mod List.");
+                serverModList = mods.Split("/@/");
+                foreach(string mod in serverModList)
+                {
+                    ModNotifyBase.logger.LogInfo(mod);
+                }
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(MenuManager), nameof(MenuManager.SetLoadingScreen))]
+        [HarmonyPostfix]
+        public static void SetLoadingScreenPatch(ref MenuManager __instance, ref RoomEnter result)
+        {
+            if (result == RoomEnter.Error && serverModList != null)
+            {
+                string[] missingMods = serverModList.Except(ModNotifyBase.ModListArray).ToArray();
+                string list = missingMods == null || missingMods.Length == 0 ? "None..?" : string.Join("\n", missingMods);
+                __instance.DisplayMenuNotification($"Failed to join modded crew!\n Missing mods:\n{list}", "[ Close ]");
+                serverModList = null;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+using HarmonyLib;
+using CompatibilityChecker.Netcode;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace CompatibilityChecker
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Lethal Company.exe")]
+    public class ModNotifyBase : BaseUnityPlugin
+    {
+        public static ModNotifyBase instance;
+        public static ManualLogSource logger;
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        public static Dictionary<string, BepInEx.PluginInfo> ModList = new Dictionary<string, BepInEx.PluginInfo>();
+        public static string ModListString;
+        public static string[] ModListArray;
+        private void Awake()
+        {
+            ModList = Chainloader.PluginInfos;
+            foreach(BepInEx.PluginInfo info in ModList.Values)
+            {
+                ModListString += $"{info.Metadata.GUID}/@/";
+            }
+            ModListString = ModListString.Remove(ModListString.Length - 3, 3); // :3
+            ModListArray = ModListString.Split("/@/");
+            if (instance == null)
+            {
+                instance = this;
+                logger = Logger;
+            }
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo("Modded servers will now notify you what mods are needed.");
+            harmony.PatchAll(typeof(ModNotifyBase));
+            harmony.PatchAll(typeof(PlayerJoinNetcode));
+        }
+    }
+}
